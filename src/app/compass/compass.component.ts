caac1b94b40085fa739b12fce5e511c2a85da9bc
Aidan Miskella <aidanmiskella10@gmail.com>
2021-02-04 22:33:43 +0000

    Added compass rotation from device orientation

--- src/app/compass/compass.component.ts
+++ src/app/compass/compass.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-compass',
@@ -7,9 +7,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CompassComponent implements OnInit {
 
+  compassStyle: any;
+  isIos = (navigator.userAgent.match(/(iPhone)/) && navigator.userAgent.match(/AppleWebKit/));
+
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+  }
+
+  onGetStartedClick() {
+    this.getPermission()
+  }
+
+  getPermission() {
+    if (this.isIos) {
+      DeviceOrientationEvent.requestPermission()
+        .then((response) => {
+          if (response === "granted") {
+            alert("iOS Permission Granted")
+          } else {
+            alert("Not Supported");
+          }
+        })
+        .catch (() => alert("Not Supported"))
+    }
+  }
+
+  @HostListener('window:deviceorientation', ['$event'])
+  getOrientation(event: DeviceOrientationEvent) {
+    if (event.alpha) {
+      var rotationDegree = Math.abs(event.alpha - 360)
+      this.compassStyle = {
+        'transform': `translate(-50%, -50%) rotate(${-rotationDegree}deg)`
+      }
+    }
   }
 
 }
